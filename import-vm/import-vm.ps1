aba2e44bc72f1efbe98549cade06e7c243b07ce6
Chris DeLashmutt <cdelashmutt@pivotal.io>
2017-11-25 15:03:45 -0500

    testing

--- import-vm/import-vm.ps1
+++ import-vm/import-vm.ps1
@@ -8,4 +8,4 @@ $cluster = Get-Cluster -Name "Cluster"
 $firstHost = Get-VMHost -Location $cluster | select -first 1
 $version = cat base-vm/version
 
-Import-vApp -Source base-vm/*.ova -VMHost $firstHost -Location $cluster -Name "windows-stemcell-$version"
+Import-vApp -Source base-vm/windows-stem-1.ova -VMHost $firstHost -Location $cluster -Name "windows-stemcell-$version"
