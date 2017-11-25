bf0da0b7d23a10ce855bdb4484bfd4cd74a2e6f4
Chris DeLashmutt <cdelashmutt@pivotal.io>
2017-11-25 14:51:42 -0500

    updating import-vm

--- import-vm/import-vm.ps1
+++ import-vm/import-vm.ps1
@@ -3,3 +3,9 @@ Get-Module -ListAvailable PowerCLI* | Import-Module
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -confirm:$false -DisplayDeprecationWarnings:$false
 
 Connect-VIServer -Server $env:VCENTER_HOST -User $env:VCENTER_USER -Password $env:VCENTER_PASSWORD
+
+$cluster = Get-Cluster -Name "Cluster"
+$firstHost = Get-VMHost -Location $cluster | select -first 1
+$version = cat base-vm/version
+
+Import-vApp -Source base-vm/*.ova -VMHost $firstHost -Location $cluster -Name "windows-stemcell-$version"
