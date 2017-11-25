989b828595ad90d79b3d9d642efd34bcb33389bc
Chris DeLashmutt <cdelashmutt@pivotal.io>
2017-11-25 15:55:19 -0500

    Updating OVA lookup

--- import-vm/import-vm.ps1
+++ import-vm/import-vm.ps1
@@ -7,6 +7,6 @@ Connect-VIServer -Server $env:VCENTER_HOST -User $env:VCENTER_USER -Password $en
 $cluster = Get-Cluster -Name "Cluster"
 $firstHost = Get-VMHost -Location $cluster | select -first 1
 $version = cat base-vm/version
-$ova = dir base-vm/*.ova | %{$_.Name}
+$ova = dir base-vm/*$version.ova | %{$_.Name}
 
 Import-vApp -Source "base-vm/$ova" -VMHost $firstHost -Location $cluster -Name "windows-stemcell-$version"
