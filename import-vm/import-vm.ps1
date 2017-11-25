a22baaf634e87006b95e310d0f028fea7d2b3840
Chris DeLashmutt <cdelashmutt@pivotal.io>
2017-11-25 15:46:28 -0500

    Variable for ova

--- import-vm/import-vm.ps1
+++ import-vm/import-vm.ps1
@@ -7,5 +7,6 @@ Connect-VIServer -Server $env:VCENTER_HOST -User $env:VCENTER_USER -Password $en
 $cluster = Get-Cluster -Name "Cluster"
 $firstHost = Get-VMHost -Location $cluster | select -first 1
 $version = cat base-vm/version
+$ova = dir base-vm/*.ova | %{$_.Name}
 
-Import-vApp -Source base-vm/windows-stem-1.ova -VMHost $firstHost -Location $cluster -Name "windows-stemcell-$version"
+Import-vApp -Source $ova -VMHost $firstHost -Location $cluster -Name "windows-stemcell-$version"
